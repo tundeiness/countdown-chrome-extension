2c965e55145442df8636a9ddf66481f127e1d14a
tundeiness <tundeiness@yahoo.co.uk>
2020-10-30 11:38:13 +0100

    create build.sh in script directory

---
+++ script/build.sh
@@ -0,0 +1,19 @@
+build() {
+    echo 'building react'
+
+    # react generate file name with hash
+    # to make sure no deprecated files in dist
+    rm -rf dist/*
+
+    # chrome extension doesn't allow inline script
+    export INLINE_RUNTIME_CHUNK=false
+    # prevent generating sourcemap
+    export GENERATE_SOURCEMAP=false
+
+    # react-scripts build generate by default PRODUCTION build
+    react-scripts build
+
+    # copy from build to dist, build could still be used by 'npm run start'
+    mkdir -p dist
+    cp -r build/* dist
+}
